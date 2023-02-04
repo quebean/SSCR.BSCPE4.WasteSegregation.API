bcca55c2053762bc2e4b603d41e622afee1a7906
quebeen <kejatori@gmail.com>
2023-02-04 13:03:48 +0800

    updated create transactions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,9 +1,13 @@
 const Transaction = require("../models/transaction");
+const User = require("../models/user");
 
 module.exports.CreateTransaction = async (req, res) => {
     const transaction = new Transaction(req.body);
     try {
+        const user = await User.findOne({tagId: transaction.tagId});
+        const points = (transaction.metal * .25)+(transaction.plastic * .15)+(transaction.paper * .05) + user.rewardBalance;
         const result = await Transaction.create(transaction);
+        await User.findOneAndUpdate({tagId: transaction.tagId, rewardBalance: points});
         res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ message: "Server Error" });
